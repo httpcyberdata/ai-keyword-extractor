4543da46cb858cfc8ebc1944e89f841f58688505
httpcyberdata <shieldedsafety@gmail.com>
2023-04-03 18:18:12 -0400

    chore: produced TextInput component w/ state, a styled button and onChange function

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Container, Box } from '@chakra-ui/react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import TextInput from './components/TextInput'
 const App = () => {
   return (
     <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
       <Container maxW='3xl' centerContent>
             <Header/> 
             <Footer/>
+            <TextInput/>
       </Container>
     </Box>
   )
