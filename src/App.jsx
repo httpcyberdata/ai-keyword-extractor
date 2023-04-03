93242f634f37dda31083191d9b1e5fe285df9d6f
httpcyberdata <shieldedsafety@gmail.com>
2023-04-03 17:57:44 -0400

    chore: produced Footer component that contains a logo image and 'Powered By AI' text

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Container, Box } from '@chakra-ui/react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 const App = () => {
   return (
     <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
       <Container maxW='3xl' centerContent>
-            <Header/>
+            <Header/> 
+            <Footer/>
       </Container>
     </Box>
   )
